d4929c59a1338514d98c18ab8f7413cd51e9302b
Edgar Ramirez <ext-edgar.1.ramirez@here.com>
2014-12-13 16:00:56 -0600

    Initial Commit

---
+++ InstaAudible/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaAudible
+//
+//  Created by Edgar Ramirez on 12/13/14.
+//  Copyright (c) 2014 Edgar Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaAudible/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaAudible
+//
+//  Created by Edgar Ramirez on 12/13/14.
+//  Copyright (c) 2014 Edgar Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
